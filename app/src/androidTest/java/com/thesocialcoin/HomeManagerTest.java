a67464e4b06b9c247dfc9e566f475b07b03698c6
Identitat ,s.l <identitat@identitat.com>
2015-08-04 10:31:41 +0200

    home manager test class + added resources endpoints strings

---
+++ app/src/androidTest/java/com/thesocialcoin/HomeManagerTest.java
@@ -0,0 +1,25 @@
+package com.thesocialcoin;
+
+import android.test.InstrumentationTestCase;
+
+import org.apache.http.client.ClientProtocolException;
+
+import java.io.IOException;
+
+/**
+ * thesocialcoin
+ * <p/>
+ * Created by identitat on 04/08/15.
+ * Copyright (c) 2015 Identitat SL. All rights reserved.
+ */
+public class HomeManagerTest extends InstrumentationTestCase {
+
+    private final static String TAG = "HomeManagerTest";
+
+    public void testGetTimeline_then200IsReceived()
+            throws ClientProtocolException, IOException {
+
+
+
+    }
+}
